c3ef0179d5bdda7da1c0eb1b7f4a7c83a3ab4114
Fede Defaveri <f.g.defaveri@gmail.com>
2017-01-02 14:25:57 +0100

    added CPM function

--- adMath.py
+++ adMath.py
@@ -1,4 +1,6 @@
-
+def calc_CPM(cpc, ctr):
+	cpm = cpc * ctr * 10
+	return cpm
 
 
 print "Hello user! This program will help you with ad Math"
@@ -7,3 +9,14 @@ print "Enter the number corresponding to what you want to calculate"
 print "[1] CPM - Cost per Mille"
 
 
+
+#get choice from user
+choice = input("Enter your choice: ")
+
+#running functions based on choice
+if 		choice == 1:
+	cpc = input("Plase input your CPC: ")
+	ctr = input("Please input your CTR: ")
+	print calc_CPM(cpc, ctr)
+elif	choice == 2:
+	#do stuff
