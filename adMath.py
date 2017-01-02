2a2f88fc22e32f6084da32b73e82bdbf470a2f05
Fede Defaveri <f.g.defaveri@gmail.com>
2017-01-02 14:20:58 +0100

    started writing code

---
+++ adMath.py
@@ -0,0 +1,9 @@
+
+
+
+print "Hello user! This program will help you with ad Math"
+print "Enter the number corresponding to what you want to calculate"
+#choices
+print "[1] CPM - Cost per Mille"
+
+
