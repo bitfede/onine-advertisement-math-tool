db2c0fd725b2ae3ab9b3c6486c650f010879b0a6
Fede Defaveri <f.g.defaveri@gmail.com>
2017-01-02 15:39:50 +0100

    added CPC calculation function

--- adMath.py
+++ adMath.py
@@ -1,22 +1,35 @@
 def calc_CPM(cpc, ctr):
-	cpm = cpc * ctr * 10
+	cpm = cpc * ctr * 10.0
 	return cpm
+
+def calc_CPC(cpm, ctr):
+	cpc = cpm / (ctr * 10.0)
+	return cpc
+
+
+def main():
+	print "Enter the number corresponding to what you want to calculate"
+	#choices
+	print "[1] CPM - Cost per Mille"
+	print "[2] CPC - Cost per Click"
+
+
+	#get choice from user
+	choice = input("Enter your choice: ")
+
+	#running functions based on choice
+	if 		choice == 1:
+		cpc = input("Plase input your CPC (example $0.50): $")
+		ctr = input("Please input your CTR in % (example 1): ")
+		cpm = calc_CPM(cpc, ctr)
+		print "$" + str(cpm)
+	elif	choice == 2:
+		cpm = input("Please input your CPM (example $5): $")
+		ctr = input("Please input your CTR in % (example 1): ")
+		cpc = calc_CPC(cpm, ctr)
+		print "$" + str(cpc)
+
 
 
 print "Hello user! This program will help you with ad Math"
-print "Enter the number corresponding to what you want to calculate"
-#choices
-print "[1] CPM - Cost per Mille"
-
-
-
-#get choice from user
-choice = input("Enter your choice: ")
-
-#running functions based on choice
-if 		choice == 1:
-	cpc = input("Plase input your CPC: ")
-	ctr = input("Please input your CTR: ")
-	print calc_CPM(cpc, ctr)
-elif	choice == 2:
-	#do stuff
+main()		
